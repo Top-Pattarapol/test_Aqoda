8666b6b7631dfcf4bd973196bc15ed37b8283f43
Pattarapol Sawasdee <pattarapol.s@kbtg.tech>
2019-09-05 15:03:00 +0700

    Initial Commit

---
+++ test_Aqoda/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test_Aqoda
+//
+//  Created by pattarapol sawasdee on 5/9/2562 BE.
+//  Copyright © 2562 pattarapol. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
